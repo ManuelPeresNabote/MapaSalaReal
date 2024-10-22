1f3f6a945cc0544a00c7f3aaee4ec909a4470cd3
ManuelPeresNabote <cabral.phs@gmail.com>
2024-10-22 10:48:14 -0300

    dsadas

--- MapaSala/Formularios/frmUsuarios.Designer.cs
+++ MapaSala/Formularios/frmUsuarios.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Usuários = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Usuários.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Usuários
+            // 
+            this.Usuários.Controls.Add(this.textBox1);
+            this.Usuários.Controls.Add(this.button1);
+            this.Usuários.Controls.Add(this.label1);
+            this.Usuários.Location = new System.Drawing.Point(12, 12);
+            this.Usuários.Name = "Usuários";
+            this.Usuários.Size = new System.Drawing.Size(776, 68);
+            this.Usuários.TabIndex = 0;
+            this.Usuários.TabStop = false;
+            this.Usuários.Text = "Usuários";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Adicionar Usuário";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pesquisar:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(276, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(482, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 352);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // frmUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmUsuarios";
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Usuários);
+            this.Name = "frmUsuarios";
+            this.Text = "Usuários";
+            this.Usuários.ResumeLayout(false);
+            this.Usuários.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox Usuários;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
